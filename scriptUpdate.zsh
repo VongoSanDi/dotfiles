32fb880e0128ef8361d99d236c5d8fb1cf562288
vongo <vongo@andres-debian.home>
2024-07-11 19:47:57 +0900

    scriptUpdate.zsh updated and works

--- scriptUpdate.zsh
+++ scriptUpdate.zsh
@@ -1,33 +1,119 @@
 #!/bin/zsh
-#This is a script that allows me to update package not installed with the apt command such as neovim and starship
 
-# Basic system update && clean
-sudo apt update
-sudo apt full-upgrade
-sudo apt autoremove
-sudo apt autoclean
-sudo apt clean
+# Define color variables
+RED="\033[0;31m" # Report cancellations or errors
+GREEN="\033[0;32m" # Confirm that certain actions were completed successfully, such as updates completed
+YELLOW="\033[1;33m" # Shows information about local and available versions, as well as confirmation requests
+BLUE="\033[0;34m" # Indicate important steps in the process, such as downloads, checkouts, and installations
 
-# We work from the ~/Downloads/
-cd ~/Downloads/
+# Function to update packages
+update_packages() {
+    echo -e "${BLUE}Updating packages ..."
+    sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove && sudo apt autoclean && sudo apt clean
+    echo
+}
 
-# Neovim update
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage && mv nvim.appimage nvim
-sudo mv nvim /usr/local/bin
-if [ $? -eq 0 ]; then
-    echo "Neovim updated and moved correctly"
+# Function to get the current installed version of Neovim
+get_local_nvim_version() {
+    nvim --version | head -n 1 | awk '{print $2}'
+}
+
+# Function to get the latest version of Neovim available on GitHub
+get_latest_nvim_version() {
+    curl --silent "https://api.github.com/repos/neovim/neovim/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+# Function to download and install the latest Neovim AppImage
+update_nvim() {
+    local latest_version=$(get_latest_nvim_version)
+    local appimage_url="https://github.com/neovim/neovim/releases/download/$latest_version/nvim.appimage"
+    local appimage_path="/usr/local/bin/nvim.appimage"
+    
+    echo -e "${BLUE}Downloading Neovim $latest_version AppImage..."
+    sudo curl -L $appimage_url -o $appimage_path
+    
+    echo -e "${BLUE}Making the AppImage executable..."
+    sudo chmod +x $appimage_path
+    
+    echo -e "${BLUE}Creating a symlink to /usr/local/bin/nvim..."
+    sudo ln -sf $appimage_path /usr/local/bin/nvim
+
+    echo -e "${GREEN}Neovim updated to version $latest_version."
+}
+
+# Function to get the current installed version of Starship
+get_local_starship_version() {
+    starship --version | awk '{print $2}'
+}
+
+# Function to get the latest version of Starship available on GitHub
+get_latest_starship_version() {
+    curl --silent "https://api.github.com/repos/starship/starship/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/'
+}
+
+# Function to download and install the latest Starship binary
+update_starship() {
+    local latest_version=$(get_latest_starship_version)
+    local tarball_url="https://github.com/starship/starship/releases/download/v$latest_version/starship-x86_64-unknown-linux-gnu.tar.gz"
+    local temp_dir=$(mktemp -d)
+    
+    echo -e "${BLUE}Downloading Starship $latest_version..."
+    curl -L $tarball_url -o $temp_dir/starship.tar.gz
+    
+    echo -e "${BLUE}Extracting the tarball..."
+    tar -xzf $temp_dir/starship.tar.gz -C $temp_dir
+    
+    echo -e "${BLUE}Installing Starship..."
+    sudo mv $temp_dir/starship /usr/local/bin/
+    
+    echo -e "${BLUE}Cleaning up..."
+    rm -rf $temp_dir
+
+    echo -e "${GREEN}Starship updated to version $latest_version."
+}
+
+# Update packages
+update_packages
+
+# Check and update Neovim
+local_nvim_version=$(get_local_nvim_version)
+latest_nvim_version=$(get_latest_nvim_version)
+
+echo -e "${YELLOW}Local Neovim version: $local_nvim_version"
+echo -e "${YELLOW}Latest Neovim version available: $latest_nvim_version"
+
+if [[ "$local_nvim_version" != "$latest_nvim_version" ]]; then
+    read -r "REPLY?${YELLOW}A new version of Neovim is available. Do you want to update Neovim? (Y/n) " REPLY
+    REPLY=${REPLY:-Y}
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        update_nvim
+    else
+        echo -e "${RED}Neovim update canceled."
+    fi
 else
-    rm nvim*
-    echo "Issue updating Neovim"
+    echo -e "${GREEN}Neovim is already up to date."
 fi
+echo
 
-# Starship update
-curl -sS https://starship.rs/install.sh | sh
-if [ $? -eq 0 ]; then
-    echo "Starship updated and moved correctly"
+# Check and update Starship
+local_starship_version=$(get_local_starship_version)
+latest_starship_version=$(get_latest_starship_version)
+
+echo -e "${YELLOW}Local Starship version: $local_starship_version"
+echo -e "${YELLOW}Latest Starship version available: $latest_starship_version"
+
+if [[ "$local_starship_version" != "$latest_starship_version" ]]; then
+    read -r "REPLY?${YELLOW}A new version of Starship is available. Do you want to update Starship? (Y/n) " REPLY
+    REPLY=${REPLY:-Y}
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        update_starship
+    else
+        echo -e "${RED}Starship update canceled."
+    fi
 else
-    echo "Issue updating Starship"
+    echo -e "${GREEN}Starship is already up to date."
 fi
+echo
 
-cd ~
+echo -e "${GREEN}Script finished."
+
